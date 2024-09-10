996d252e78427b8f6864f28af6954cc13ba2d9a7
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-09-10 22:20:20 +0200

    style: changed the background of the input and textarea from shadcn-vue

--- frontend/src/components/ui/textarea/Textarea.vue
+++ frontend/src/components/ui/textarea/Textarea.vue
@@ -20,5 +20,6 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 </script>
 
 <template>
-  <textarea v-model="modelValue" :class="cn('flex min-h-[60px] w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300', props.class)" />
+  <textarea v-model="modelValue"
+    :class="cn('flex min-h-[60px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm shadow-sm placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300', props.class)" />
 </template>
