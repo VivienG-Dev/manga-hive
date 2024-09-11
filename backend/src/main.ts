fd8fc0123b4a86d110c264a8b48f7e16c1954e88
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-09-11 19:18:05 +0200

    feat: added the possibility to read content from the new upload folder

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.enableCors({
     origin: true, // This allows all origins
@@ -20,6 +22,12 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  // Serve static files from 'uploads' directory
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
   await app.listen(3000);
 }
 bootstrap();
