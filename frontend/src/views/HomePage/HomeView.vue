d61d4d00f2d1ca2db1a4575245fe8b375a6aa253
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-10-04 15:47:51 +0200

    refactor: remove unused component

--- frontend/src/views/HomePage/HomeView.vue
+++ frontend/src/views/HomePage/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/views/HomePage/components/TheWelcome.vue'
 import SearchBar from './components/SearchBar.vue'
 </script>
 
 <template>
   <div class="container mx-auto px-4 py-8 space-y-16 my-8">
-    <TheWelcome />
     <SearchBar />
   </div>
 </template>
--- frontend/src/views/HomePage/components/TheWelcome.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div class="text-1xl font-bold mt-8 w-72">This is main page where you can get
-    the data from the api</div>
-</template>
