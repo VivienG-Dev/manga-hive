87bf69bccbb9d1d3c7d6fb2b2056c82c1d5e27f6
VivienG-Dev <77700655+VivienG-Dev@users.noreply.github.com>
2024-10-23 13:59:59 +0200

    fix: commented code that is not used anymore

--- frontend/src/views/SignUpPage/components/CreateForm.vue
+++ frontend/src/views/SignUpPage/components/CreateForm.vue
@@ -19,7 +19,7 @@ import { AxiosError } from 'axios'
 
 const router = useRouter()
 const authStore = useAuthStore()
-const { user } = storeToRefs(authStore)
+// const { user } = storeToRefs(authStore)
 
 // Define the schema for our form
 const schema = z.object({
@@ -141,10 +141,8 @@ async function signup() {
         </Button>
         <Button class="bg-white dark:text-background" variant="outline">
           <svg role="img" viewBox="0 0 24 24" class="mr-2 h-4 w-4">
-            <path
-              fill="currentColor"
-              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-            />
+            <path fill="currentColor"
+              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
           </svg>
           Google
         </Button>
@@ -160,39 +158,22 @@ async function signup() {
       <!-- Email input -->
       <div class="grid gap-2">
         <Label for="email">Email <span class="text-red-500">*</span></Label>
-        <Input
-          class="bg-white text-foreground dark:text-background"
-          :class="{ 'border-red-500': emailError }"
-          id="email"
-          type="email"
-          v-model="email"
-          placeholder="m@example.com"
-        />
+        <Input class="bg-white text-foreground dark:text-background" :class="{ 'border-red-500': emailError }"
+          id="email" type="email" v-model="email" placeholder="m@example.com" />
         <div v-if="emailError" class="text-red-500 text-sm">{{ emailError }}</div>
       </div>
       <!-- Email input -->
       <div class="grid gap-2">
         <Label for="email">Username <span class="text-red-500">*</span></Label>
-        <Input
-          class="bg-white text-foreground dark:text-background"
-          :class="{ 'border-red-500': usernameError }"
-          id="username"
-          type="username"
-          v-model="username"
-          placeholder="Username"
-        />
+        <Input class="bg-white text-foreground dark:text-background" :class="{ 'border-red-500': usernameError }"
+          id="username" type="username" v-model="username" placeholder="Username" />
         <div v-if="usernameError" class="text-red-500 text-sm">{{ usernameError }}</div>
       </div>
       <!-- Password input -->
       <div class="grid gap-2">
         <Label for="password">Password <span class="text-red-500">*</span></Label>
-        <Input
-          class="bg-white text-foreground dark:text-background"
-          :class="{ 'border-red-500': passwordError }"
-          id="password"
-          type="password"
-          v-model="password"
-        />
+        <Input class="bg-white text-foreground dark:text-background" :class="{ 'border-red-500': passwordError }"
+          id="password" type="password" v-model="password" />
         <div v-if="passwordError" class="text-red-500 text-sm">{{ passwordError }}</div>
       </div>
       <!-- General error message -->
